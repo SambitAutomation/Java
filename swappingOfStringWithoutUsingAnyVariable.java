a466f9a9f2324a2adb57c5c6e6e4e433f7db6148
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2022-09-01 22:19:47 +0530

    Create swappingOfStringWithoutUsingAnyVariable.java

---
+++ swappingOfStringWithoutUsingAnyVariable.java
@@ -0,0 +1,20 @@
+class swappingOfStringWithoutUsingAnyVariable
+{
+public static void main()
+{
+String a = "Abc";
+String b = "Xzy";
+
+a=a+b;
+
+b=b.substring(0, a.length()-b.substring());
+
+a=a.substring(b.length());
+
+System.out.println("Ist string value : - " + a);
+
+System.out.println("2nd string value : - " + b);
+
+}
+}
+
