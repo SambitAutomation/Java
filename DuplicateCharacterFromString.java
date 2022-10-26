096923ad4f58dfa73e9914104acbe6b96cef548f
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2022-10-26 20:38:48 +0530

    Create DuplicateCharacterFromString.java

---
+++ DuplicateCharacterFromString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class DuplicateCharacterFromString
+{
+public static void main(String[] args)
+{
+String s = "teastaing";
+
+char[] ch = s.toCharArray();
+
+for(int i=0; i<s.length(); i++)
+{
+  for(int j = i+1; j<s.length(); j++)
+ {
+       if(ch[i] == ch[j])
+                  {
+                                  System.out.println(ch[j]);
+}
+}
+}
+}
+}
