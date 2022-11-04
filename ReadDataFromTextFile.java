7aa927a783d75bfae7c6c446103e77ff0d04ccd1
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2022-11-04 14:50:10 +0530

    Update ReadDataFromTextFile.java

--- ReadDataFromTextFile.java
+++ ReadDataFromTextFile.java
@@ -5,7 +5,7 @@ class ReadDataFromTextFile
 public static void main(String[] args)throws Exception
 {
 File f=new File("D:\\t123.txt");
-Scanner sc=new Scanner(f);
+Scanner sc=new Scanner(f); 
 
 while(sc.hasNextLine())
 {
@@ -13,4 +13,4 @@ System.out.println(sc.nextLine());
 }
 
 }
-}
+} 
