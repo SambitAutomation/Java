0209790ab9ddad71ae7760c13f2785e198fd6b6a
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2023-05-10 15:10:31 +0530

    Update NumberToStringConversion.java

--- NumberToStringConversion.java
+++ NumberToStringConversion.java
@@ -14,3 +14,63 @@ class NumberToStringConversion {
         
     }
 }
+
+---------------------------------------------------------------------------------------------------------------------
+    Using Switch statement
+    =======================
+    
+    class NumberToStringConversion {
+     public static void main(String[] args) {
+        
+        String a = "92823";
+        
+        for(int i=0;i<a.length();i++)
+        {
+            switch(a.charAt(i))
+            {
+                case '0':
+                    System.out.println("zero");
+                    break;
+                    
+                case '1':
+                    System.out.println("one");
+                    break;
+                    
+                case '2':
+                    System.out.println("two");
+                    break; 
+                    
+                case '3':
+                    System.out.println("three");
+                    break;
+                    
+                case '4':
+                    System.out.println("four");
+                    break;
+                    
+                case '5':
+                    System.out.println("five");
+                    break; 
+                    
+                 case '6':
+                    System.out.println("six");
+                    break;
+                    
+                case '7':
+                    System.out.println("seven");
+                    break; 
+                    
+                case '8':
+                    System.out.println("eight");
+                    break;
+                    
+                case '9':
+                    System.out.println("nine");
+                    break;
+                    
+               
+            }
+        }
+        
+    }
+}
